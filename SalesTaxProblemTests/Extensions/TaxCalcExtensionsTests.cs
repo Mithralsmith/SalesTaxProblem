56987004833da873b30e6e941b5e8d34d2196a94
Ben Bolton <microsoft@mithralsmith.com>
2020-11-08 15:06:37 -0800

    Added Additional rounding scenarios to the tests.

--- SalesTaxProblemTests/Extensions/TaxCalcExtensionsTests.cs
+++ SalesTaxProblemTests/Extensions/TaxCalcExtensionsTests.cs
@@ -19,6 +19,29 @@ namespace SalesTaxProblem.Extensions.Tests
             new object[] {TaxConstants.FiveCents, 0.04999, 0.05},
             new object[] {TaxConstants.FiveCents, 0.00001, 0.05},
             new object[] {TaxConstants.FiveCents, 0.00, 0.00},
+
+            new object[] {0, 0.05, 0.05},
+            new object[] {0, 0.05999, 0.05999},
+            new object[] {0, 0.05001, 0.05001},
+            new object[] {0, 0.0525, 0.0525},
+            new object[] {0, 0.04999, 0.04999},
+            new object[] {0, 0.00001, 0.00001},
+            new object[] {0, 0.00, 0.00},
+
+            new object[] {.01, 0.05, 0.05},
+            new object[] {.01, 0.05999, 0.06},
+            new object[] {.01, 0.05001, 0.06},
+            new object[] {.01, 0.0525, 0.06},
+            new object[] {.01, 0.04999, 0.05},
+            new object[] {.01, 0.00001, 0.01},
+            new object[] {.01, 0.00, 0.00},
+
+            new object[] {.1, 0.10, 0.10},
+            new object[] {.1, 0.1999, 0.20},
+            new object[] {.1, 0.0999, 0.10},
+            new object[] {.1, 0.15, 0.20},
+            new object[] {.1, 0.00001, 0.10},
+            new object[] {.1, 0.00, 0.00},
         };
 
         [Test, TestCaseSource(nameof(_roundUpTo5CentsStartExpectedCases))]
